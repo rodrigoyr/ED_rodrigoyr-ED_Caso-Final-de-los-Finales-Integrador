cd2cf720feec0ad1b852a8f737b6b995ca986dbd
rodrigoyr <ryeperub@myuax.com>
2024-05-08 11:39:38 +0200

    commit ej5

--- Ejercicio 5/src/main/java/Main.java
+++ Ejercicio 5/src/main/java/Main.java
@@ -1,1 +1,23 @@
 
+import model.*;
+import exceptions.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UserAccount sender = new UserAccount("Sender");
+            UserAccount recipient = new UserAccount("Recipient");
+
+            Tweet tweet = new Tweet(sender, "This is a tweet");
+            System.out.println(tweet);
+
+            Retweet retweet = new Retweet(sender, "This is a retweet", tweet);
+            System.out.println(retweet);
+
+            DirectMessage directMessage = new DirectMessage(sender, "This is a direct message", recipient);
+            System.out.println(directMessage);
+        } catch (TweetTooLongException | InvalidEmailException | InvalidAliasException e) {
+            e.printStackTrace();
+        }
+    }
+}
