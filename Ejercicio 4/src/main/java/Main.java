c83a671b218ab66b6ee266973c8939ba73045f8c
rodrigoyr <ryeperub@myuax.com>
2024-05-08 11:06:31 +0200

    commit ej4

---
+++ Ejercicio 4/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 102;
+        int result = sumOfDigits(number);
+        System.out.println("La suma de los dígitos de " + number + " es " + result);
+    }
+
+    public static int sumOfDigits(int n) {
+        if (n < 10) return n;
+        return n % 10 + sumOfDigits(n / 10);
+    }
+}
