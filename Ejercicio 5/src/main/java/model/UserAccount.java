424cfd4ecf8953319a56112b39afe8eb447f24cd
rodrigoyr <ryeperub@myuax.com>
2024-05-08 11:28:49 +0200

    commit ej5

--- Ejercicio 5/src/main/java/model/UserAccount.java
+++ Ejercicio 5/src/main/java/model/UserAccount.java
@@ -8,28 +8,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserAccount {
-    public class UserAccount {
-        private String alias;
-        private String email;
-        private List<Tweet> tweets;
-        private List<Tweet> timeline;
-        private List<UserAccount> followers;
-        private List<UserAccount> following;
+    private String alias;
+    private String email;
+    private List<Tweet> tweets;
+    private List<Tweet> timeline;
+    private List<UserAccount> followers;
+    private List<UserAccount> following;
 
-        public UserAccount(String alias, String email) throws InvalidEmailException, InvalidAliasException {
-            if (!Utils.isValidEmail(email)) {
-                throw new InvalidEmailException("Invalid email");
-            }
-            if (!Utils.isValidAlias(alias)) {
-                throw new InvalidAliasException("Invalid alias");
-            }
-            this.alias = alias;
-            this.email = email;
-            this.tweets = new ArrayList<>();
-            this.timeline = new ArrayList<>();
-            this.followers = new ArrayList<>();
-            this.following = new ArrayList<>();
+    public UserAccount(String alias, String email) throws InvalidEmailException, InvalidAliasException {
+        if (!Utils.isValidEmail(email)) {
+            throw new InvalidEmailException("Invalid email");
         }
+        if (!Utils.isValidAlias(alias)) {
+            throw new InvalidAliasException("Invalid alias");
+        }
+        this.alias = alias;
+        this.email = email;
+        this.tweets = new ArrayList<>();
+        this.timeline = new ArrayList<>();
+        this.followers = new ArrayList<>();
+        this.following = new ArrayList<>();
+    }
 
     public void follow(UserAccount user) {
         if (this.following.contains(user)) {
